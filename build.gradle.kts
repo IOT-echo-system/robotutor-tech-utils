8ae7f04fb73e73ca33548cf94d33a657fd203236
Shivam Rajput <raj.shiviraj@gmail.com>
2024-10-29 09:12:42 +0530

    Shivi | Add base url.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ publishing {
             from(components["java"])
             groupId = "com.robotutor"
             artifactId = "robotutor-tech-utils"
-            version = "1.0.0"
+            version = "1.0.1"
 
             pom {
                 name.set("Reactive Logging Starter")
