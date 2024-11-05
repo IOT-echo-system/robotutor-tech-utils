a17e7ea11a8e24c8000f3b14df83dbe5f88aafba
Shivam Rajput <raj.shiviraj@gmail.com>
2024-11-05 10:22:16 +0530

    Shivi | update package name.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ publishing {
             from(components["java"])
             groupId = "com.robotutor"
             artifactId = "robotutor-tech-utils"
-            version = "1.0.5"
+            version = "1.0.6"
 
             pom {
                 name.set("Common Reactive Utils library")
