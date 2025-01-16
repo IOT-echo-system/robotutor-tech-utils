bb7819aab8aa3e0a2bb92500e5f5ed8b8306104b
Shivam Rajput <raj.shiviraj@gmail.com>
2025-01-16 19:21:26 +0530

    Shivi | updated redis-starter version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ publishing {
             from(components["java"])
             groupId = "com.robotutor"
             artifactId = "robotutor-tech-utils"
-            version = "1.0.38"
+            version = "1.0.39"
 
             artifact(tasks["testJar"])
 
